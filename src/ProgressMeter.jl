c842e646ce143aacec2bfbe8cd3a97924ce8d96e
Alan Bahm <alanbahm@gmail.com>
2013-08-29 23:54:35 -0700

    Adding features showing ETA, final time, and optional meter
    Adding a roughly calculated ETA and final time to 100%.
    Optionally, the user may specify a meter M characters
    long, made of hash signs and spaces '#########        '
    Added an additional test, and modified description

--- src/ProgressMeter.jl
+++ src/ProgressMeter.jl
@@ -7,16 +7,23 @@ type Progress
     dt::Float64
     counter::Int
     inext::Int
+    tfirst::Float64
     tlast::Float64
     printed::Bool    # true if we have issued at least one status update
+    desc::String
+    barlen::Int
     
-    function Progress(n::Integer, dt::Real = 1.0)
+    function Progress(n::Integer, dt::Real = 1.0, desc::String = "Progress: ", barlen::Int = 0)
         this = new(convert(Int, n), convert(Float64, dt), 0)
-        this.inext = iceil(n/100)
-        this.tlast = time()
+        this.inext = ceil(n/100)
+        this.tfirst = time()
+        this.tlast = this.tfirst
         this.printed = false
+        this.desc = desc
+        this.barlen = barlen
         this
     end
+
 end
 
 function next!(p::Progress)
@@ -24,7 +31,10 @@ function next!(p::Progress)
     p.counter += 1
     if p.counter >= p.n
         if p.printed
-            printover("Progress: done", :green)
+            percentage_complete = 100.0 * p.counter / p.n
+            bar = print_bar(p.barlen, percentage_complete)
+            dur = print_duration(t-p.tfirst)
+            printover(string(p.desc, iround(percentage_complete), "%", bar, " Time: ", dur), :green)
             println()
         end
         return
@@ -32,7 +42,13 @@ function next!(p::Progress)
     if p.counter >= p.inext
         p.inext += iceil(p.n/100)
         if t > p.tlast+p.dt
-            printover(string("Progress: ", iround(100*p.counter/p.n), "%"), :green)
+            percentage_complete = 100.0 * p.counter / p.n
+            bar = print_bar(p.barlen, percentage_complete)
+            elapsed_time = t - p.tfirst
+            est_total_time = 100 * elapsed_time / percentage_complete
+            eta_sec = iround( est_total_time - elapsed_time )
+            eta = print_duration(eta_sec)
+            printover(string(p.desc, iround(percentage_complete), "%", bar, "  ETA: ", eta), :green)
             # Compensate for any overhead of printing. This can be especially important
             # if you're running over a slow network connection.
             p.tlast = t + 2*(time()-t)
@@ -56,5 +72,30 @@ function printover(io::IO, s::String, color::Symbol = color_normal)
 end
 
 printover(s::String, color::Symbol = color_normal) = printover(STDOUT, s, color)
+
+function print_bar(barlen, percentage_complete)
+    bar = ""
+    if barlen>0
+        nsolid = iround(barlen * percentage_complete / 100)
+        nempty = barlen - nsolid
+        bar = string("|", repeat("#",nsolid), repeat(" ",nempty), "|")
+    end
+    return bar
+end
+
+function print_duration(nsec)
+    days = div(nsec, 60*60*24)
+    r = nsec - 60*60*24*days
+    hours = div(r,60*60)
+    r = r - 60*60*hours
+    minutes = div(r, 60)
+    seconds = r - 60*minutes
+
+    hhmmss = @sprintf "%u:%02u:%02u" hours minutes seconds
+    if days>0
+       return @sprintf "%u days, %s" days hhmmss
+    end
+    return hhmmss
+end
 
 end
