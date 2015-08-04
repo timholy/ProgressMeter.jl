timholy/ProgressMeter.jl

commit 8899eb76e1216e37f71fbd7946c08cd999ff5184

Refactor @showprogress macro to support multidimensional loops
Closes #23.

src/ProgressMeter.jl
@@ -153,19 +153,34 @@ function showprogress_process_expr(node, metersym)
             ($finish!)($metersym)
             $node
         end
-    elseif node.head === :continue
-        # special handling for continue statement
-        quote
-            ($next!)($metersym)
-            $node
-        end
     elseif node.head === :for || node.head === :while
-        # do not process break and continue statements in inner loops
+        # do not process inner loops
+        #
+        # FIXME: do not process break and return statements in inner functions
+        # either
         node
     else
         # process each subexpression recursively
         Expr(node.head, [showprogress_process_expr(a, metersym) for a in node.args]...)
     end
+end
+
+immutable ProgressWrapper{T}
+    obj::T
+    meter::Progress
+end
+
+ProgressWrapper{T}(obj::T, meter::Progress) = ProgressWrapper{T}(obj, meter)
+
+Base.length(wrap::ProgressWrapper) = Base.length(wrap.obj)
+Base.start(wrap::ProgressWrapper) = (Base.start(wrap.obj), true)
+Base.done(wrap::ProgressWrapper, state) = Base.done(wrap.obj, state[1])
+
+function Base.next(wrap::ProgressWrapper, state)
+    st, firstiteration = state
+    firstiteration || next!(wrap.meter)
+    i, st = Base.next(wrap.obj, st)
+    return (i, (st, false))
 end
 
 macro showprogress(args...)
@@ -177,67 +192,66 @@ macro showprogress(args...)
     metersym = gensym("meter")
 
     if isa(loop, Expr) && loop.head === :for
-        @assert length(loop.args) == 2
-        assignidx = 1
-        loopbodyidx = 2
-        is_comprehension = false
+        outerassignidx = 1
+        loopbodyidx = endof(loop.args)
     elseif isa(loop, Expr) && loop.head in (:comprehension, :dict_comprehension)
-        @assert length(loop.args) == 2
-        assignidx = 2
+        outerassignidx = endof(loop.args)
         loopbodyidx = 1
-        is_comprehension = true
     elseif isa(loop, Expr) && loop.head in (:typed_comprehension, :typed_dict_comprehension)
-        @assert length(loop.args) == 3
-        assignidx = 3
+        outerassignidx = endof(loop.args)
         loopbodyidx = 2
-        is_comprehension = true
     else
         throw(ArgumentError("Final argument to @showprogress must be a for loop or comprehension."))
     end
 
-    newloop = Expr(loop.head, loop.args...)
+    loop = copy(loop)
 
-    # Transform the loop body
-    is_dict_comprehension = loop.head in (:dict_comprehension, :typed_dict_comprehension)
-    if is_dict_comprehension
-        @assert loop.args[loopbodyidx].head === :(=>)
-        @assert length(loop.args[loopbodyidx].args) == 2
-        innerbody = loop.args[loopbodyidx].args[2]
-    else
-        innerbody = loop.args[loopbodyidx]
+    # Transform the first loop assignment
+    loopassign = loop.args[outerassignidx] = copy(loop.args[outerassignidx])
+    if loopassign.head === :block # this will happen in a for loop with multiple iteration variables
+        for i in 2:length(loopassign.args)
+            loopassign.args[i] = esc(loopassign.args[i])
+        end
+        loopassign = loopassign.args[1] = copy(loopassign.args[1])
     end
-    if is_comprehension
-        newinnerbody = quote
+    @assert loopassign.head === :(=)
+    @assert length(loopassign.args) == 2
+    obj = loopassign.args[2]
+    loopassign.args[1] = esc(loopassign.args[1])
+    loopassign.args[2] = :(ProgressWrapper(iterable, $(esc(metersym))))
+
+    # Transform the loop body break and return statements
+    if loop.head === :for
+        loop.args[loopbodyidx] = showprogress_process_expr(loop.args[loopbodyidx], metersym)
+    end
+
+    # Escape all args except the loop assignment, which was already appropriately escaped.
+    for i in 1:length(loop.args)
+        if i != outerassignidx
+            loop.args[i] = esc(loop.args[i])
+        end
+    end
+
+    setup = quote
+        iterable = $(esc(obj))
+        $(esc(metersym)) = Progress(length(iterable), $([esc(arg) for arg in progressargs]...))
+    end
+
+    if loop.head === :for
+        return quote
+            $setup
+            $loop
+        end
+    else
+        # We're dealing with a comprehension
+        return quote
             begin
-                rv = $(esc(showprogress_process_expr(innerbody, metersym)))
-                $(next!)($(esc(metersym)))
+                $setup
+                rv = $loop
+                next!($(esc(metersym)))
                 rv
             end
         end
-    else
-        newinnerbody = quote
-            begin
-                $(esc(showprogress_process_expr(innerbody, metersym)))
-                $(next!)($(esc(metersym)))
-            end
-        end
-    end
-    if is_dict_comprehension
-        newloop.args[loopbodyidx] = Expr(:(=>), esc(loop.args[loopbodyidx].args[1]), newinnerbody)
-    else
-        newloop.args[loopbodyidx] = newinnerbody
-    end
-
-    # Transform the loop assignment
-    loopassign = loop.args[assignidx]
-    @assert loopassign.head == :(=)
-    @assert length(loopassign.args) == 2
-    newloop.args[assignidx] = :($(esc(loopassign.args[1])) = iterable)
-
-    return quote
-        iterable = $(esc(loopassign.args[2]))
-        $(esc(metersym)) = Progress(length(iterable), $([esc(arg) for arg in progressargs]...))
-        $newloop
     end
 end
 

test/test.jl
@@ -1,6 +1,8 @@
 import ProgressMeter
 import Base.Test.@test
 
+using Compat
+
 function testfunc(n, dt, tsleep)
     p = ProgressMeter.Progress(n, dt)
     for i = 1:n
@@ -80,8 +82,21 @@ function testfunc6(n, dt, tsleep)
     end
 end
 
+function testfunc6a(n, dt, tsleep)
+    ProgressMeter.@showprogress dt for i in 1:n, j in 1:n
+        if i == div(n, 2)
+            break
+        end
+        if !isprime(i)
+            sleep(tsleep)
+            continue
+        end
+    end
+end
+
 println("Testing @showprogress macro on for loop")
 testfunc6(3000, 0.01, 0.002)
+testfunc6a(30, 0.01, 0.002)
 
 
 function testfunc7(n, dt, tsleep)
@@ -89,8 +104,14 @@ function testfunc7(n, dt, tsleep)
     @test s == [1:n;]
 end
 
+function testfunc7a(n, dt, tsleep)
+    s = ProgressMeter.@showprogress dt "Calculating..." [(sleep(tsleep); z) for z in 1:n, y in 1:n]
+    @test s == [z for z in 1:n, y in 1:n]
+end
+
 println("Testing @showprogress macro on comprehension")
-testfunc7(100, 0.1, 0.01)
+testfunc7(25, 0.1, 0.1)
+testfunc7a(5, 0.1, 0.1)
 
 
 function testfunc8(n, dt, tsleep)
@@ -122,8 +143,14 @@ function testfunc9(n, dt, tsleep)
     @test s == [1:n;]
 end
 
+function testfunc9a(n, dt, tsleep)
+    s = ProgressMeter.@showprogress dt "Calculating..." Float64[(sleep(tsleep); z) for z in 1:n, y in 1:n]
+    @test s == [z for z in 1:n, y in 1:n]
+end
+
 println("Testing @showprogress macro on typed comprehension")
 testfunc9(100, 0.1, 0.01)
+testfunc9a(10, 0.1, 0.01)
 
 
 function testfunc10(n, k, dt, tsleep)
@@ -146,8 +173,15 @@ function testfunc11(n, dt, tsleep)
     @test s == [z => 2z for z in 1:n]
 end
 
+function testfunc11a(n, dt, tsleep)
+    f(x) = (sleep(tsleep); 2x)
+    s = ProgressMeter.@showprogress dt "Calculating..." [(y,z) => f(z) for z in 1:n, y in 1:n]
+    @test s == [(y,z) => 2z for z in 1:n, y in 1:n]
+end
+
 println("Testing @showprogress macro on dict comprehension")
-testfunc11(100, 0.1, 0.01)
+testfunc11(100, 0.1, 0.1)
+testfunc11a(10, 0.1, 0.1)
 
 
 function testfunc12(n, dt, tsleep)
@@ -156,8 +190,15 @@ function testfunc12(n, dt, tsleep)
     @test s == (Int=>Int)[z => 2z for z in 1:n]
 end
 
+function testfunc12a(n, dt, tsleep)
+    f(x) = (sleep(tsleep); 2x)
+    s = ProgressMeter.@showprogress dt "Calculating..." (@compat(Tuple{Int,Int})=>Int)[(y,z) => f(z) for z in 1:n, y in 1:n]
+    @test s == (@compat(Tuple{Int,Int})=>Int)[(y,z) => 2z for z in 1:n, y in 1:n]
+end
+
 println("Testing @showprogress macro on typed dict comprehension")
-testfunc12(100, 0.1, 0.01)
+testfunc12(100, 0.1, 0.1)
+testfunc12a(10, 0.1, 0.1)
 
 
 function testfunc13()

commit 7300ec078798dfffb25561f0ab0f7dd0c29405f3

Fix regression in @showprogress
8899eb7 broke @showprogress such that the final next!() was no
longer called, and the progress meter would never consider itself
complete.  This fixes that by calling finish!() explicitly when
the outer loop is complete.

src/ProgressMeter.jl
@@ -174,7 +174,12 @@ ProgressWrapper{T}(obj::T, meter::Progress) = ProgressWrapper{T}(obj, meter)
 
 Base.length(wrap::ProgressWrapper) = Base.length(wrap.obj)
 Base.start(wrap::ProgressWrapper) = (Base.start(wrap.obj), true)
-Base.done(wrap::ProgressWrapper, state) = Base.done(wrap.obj, state[1])
+
+function Base.done(wrap::ProgressWrapper, state)
+    done = Base.done(wrap.obj, state[1])
+    done && finish!(wrap.meter)
+    return done
+end
 
 function Base.next(wrap::ProgressWrapper, state)
     st, firstiteration = state