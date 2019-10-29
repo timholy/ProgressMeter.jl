237365084b3fd04bd1c8446d5dd225ebf6be86df
Tim Holy <tim.holy@gmail.com>
2019-10-29 13:43:46 -0500

    Restore tests commented-out in da0498b1428de1bdbabdc171636cd2f856149865

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,11 @@
 import ProgressMeter
 using Test
 
-# include("core.jl")
-# include("test.jl")
-# include("test_showvalues.jl")
+include("core.jl")
+include("test.jl")
+include("test_showvalues.jl")
 include("test_map.jl")
-# include("test_float.jl")
+include("test_float.jl")
 
 println("")
 println("All tests complete")
