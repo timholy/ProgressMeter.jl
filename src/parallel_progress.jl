5f2b17cb9b3cb80edb596a03d6190728fdc97e8e
Marc Ittel <marc.ittel@gmail.com>
2021-08-12 16:00:46 +0200

    add parallel support for ProgressTresh and ProgressUnknown

--- src/parallel_progress.jl
+++ src/parallel_progress.jl
@@ -26,21 +26,21 @@ const PP_CANCEL = :cancel
 const PP_FINISH = :finish
 const PP_UPDATE = :update
 
-next!(pp::ParallelProgress, args...; kw...) = put!(pp.channel, (PP_NEXT, args, kw))
-cancel(pp::ParallelProgress, args...; kw...) = put!(pp.channel, (PP_CANCEL, args, kw))
-finish!(pp::ParallelProgress, args...; kw...) = put!(pp.channel, (PP_FINISH, args, kw))
-update!(pp::ParallelProgress, args...; kw...) = put!(pp.channel, (PP_UPDATE, args, kw))
+next!(pp::ParallelProgress, args...; kw...) = (put!(pp.channel, (PP_NEXT, args, kw)); nothing)
+cancel(pp::ParallelProgress, args...; kw...) = (put!(pp.channel, (PP_CANCEL, args, kw)); nothing)
+finish!(pp::ParallelProgress, args...; kw...) = (put!(pp.channel, (PP_FINISH, args, kw)); nothing)
+update!(pp::ParallelProgress, args...; kw...) = (put!(pp.channel, (PP_UPDATE, args, kw)); nothing)
 
 function ParallelProgress(n::Integer; kw...)
     return ParallelProgress(Progress(n; kw...); kw...)
 end
 
-function ParallelProgress(progress::Progress)
-    channel = RemoteChannel(() -> Channel{Tuple}())
+function ParallelProgress(progress::AbstractProgress)
+    channel = RemoteChannel(() -> Channel{NTuple{3,Any}}(1024))
     pp = ParallelProgress(channel)
     
-    @async begin 
-        while progress.counter < progress.n
+    @async begin
+        while !has_finished(progress)
             f, args, kw = take!(channel)
             if f == PP_NEXT
                 next!(progress, args...; kw...)
@@ -53,18 +53,22 @@ function ParallelProgress(progress::Progress)
             elseif f == PP_UPDATE
                 update!(progress, args...; kw...)
             else
-                error("not recognized: $f")
+                error("not recognized: $(repr(f))")
             end
         end
-        # empty channel before ending it
-        while isready(pp.channel)
-            take!(pp.channel)
-        end
+        
         pp.channel = FakeChannel()
+        while isready(channel)
+            take!(channel)
+        end
+        close!(channel)
     end
-
     return pp
 end
+
+has_finished(p::Progress) = p.counter >= p.n
+has_finished(p::ProgressThresh) = p.triggered
+has_finished(p::ProgressUnknown) = p.done
 
 # fake channel to allow over-shoot
 struct FakeChannel end
@@ -85,8 +89,6 @@ end
 
 Base.getindex(mp::MultipleProgress, n) = ParallelProgress.(MultipleChannel.(Ref(mp.channel), n))
 Base.lastindex(mp::MultipleProgress) = mp.amount
-finish!(mp::MultipleProgress, args...; kw...) = finish!.(mp[1:end])
-cancel(mp::MultipleProgress, args...; kw...) = cancel.(mp[1:end])
 
 """
     prog = MultipleProgress(amount, lengths; kw...)
@@ -125,92 +127,106 @@ julia> p = MultipleProgress(5,10; desc="global ", kws=[(desc="task \$i ",) for i
 ```
 """
 function MultipleProgress(lengths::AbstractVector{<:Integer}; 
-                          count_overshoot::Bool = false,
+                          count_overshoot = false,
+                          mainprogress = true,
                           kws = [() for _ in lengths],
                           kw...)
     @assert length(lengths) == length(kws) "`length(lengths)` must be equal to `length(kws)`"
     amount = length(lengths)
 
     total_length = sum(lengths)
-    main_progress = Progress(total_length; offset=0, kw...)
+    mainprogress && (main_progress = Progress(total_length; offset=0, kw...))
     progresses = Union{Progress,Nothing}[nothing for _ in 1:amount]
     taken_offsets = Set{Int}()
-    channel = RemoteChannel(() -> Channel{Tuple}())
+    mainprogress && push!(taken_offsets, 0)
+    channel = RemoteChannel(() -> Channel{NTuple{4,Any}}())
 
-    max_offsets = 1
+    max_offsets = 0
 
     mp = MultipleProgress(channel, amount, collect(lengths))
 
     # we must make sure that 2 progresses aren't updated at the same time, 
     # that's why we use only one Channel
     @async begin
-        while main_progress.counter < total_length
-            
-            p, f, args, kw = take!(channel)
+        try
+            while main_progress.counter < total_length
+                
+                p, f, args, kw = take!(channel)
 
-            # main progressbar
-            if p == 0
-                if f == PP_CANCEL
-                    cancel(main_progress, args...; kw...)
-                    break
-                elseif f == PP_UPDATE
-                    update!(main_progress, args...; kw...)
-                elseif f == PP_NEXT
-                    next!(main_progress, args...; kw...)
-                elseif f == PP_FINISH
-                    finish!(main_progress, args...; kw...)
-                end
-            else
-
-                # first time calling progress p
-                if isnothing(progresses[p])
-                    # find first available offset
-                    offset = 1
-                    while offset in taken_offsets
-                        offset += 1
-                    end
-                    max_offsets = max(max_offsets, offset)
-                    progresses[p] = Progress(lengths[p]; offset=offset, kw..., kws[p]...)
-                    push!(taken_offsets, offset)
-                end
-
-
-                if f == PP_NEXT
-                    if count_overshoot || progresses[p].counter < lengths[p]
-                        next!(progresses[p], args...; kw...)
-                        next!(main_progress)
+                # main progressbar
+                if p == 0
+                    mainprogress || continue
+                    if f == PP_CANCEL
+                        cancel(main_progress, args...; kw...)
+                        break
+                    elseif f == PP_UPDATE
+                        if !isempty(args) && args[1] == (:)
+                            update!(main_progress, main_progress.counter, args[2:end]...; kw...)
+                        else
+                            update!(main_progress, args...; kw...)
+                        end
+                    elseif f == PP_NEXT
+                        next!(main_progress, args...; kw...)
+                    elseif f == PP_FINISH
+                        finish!(main_progress, args...; kw...)
                     end
                 else
-                    prev_p_value = progresses[p].counter
-                    
-                    if f == PP_FINISH
-                        finish!(progresses[p], args...; kw...)
-                    elseif f == PP_CANCEL
-                        finish!(progresses[p])
-                        cancel(progresses[p], args...; kw...)
-                    elseif f == PP_UPDATE
-                        if !count_overshoot && !isempty(args)
-                            value = min(args[1], lengths[n])
-                            update!(progresses[p], value, args[2:end]...; kw...)
-                        else
-                            update!(progresses[p], args...; kw...)
+
+                    # first time calling progress p
+                    if isnothing(progresses[p])
+                        # find first available offset
+                        offset = 0
+                        while offset in taken_offsets
+                            offset += 1
                         end
-                        
+                        max_offsets = max(max_offsets, offset)
+                        progresses[p] = Progress(lengths[p]; offset=offset, kws[p]...)
+                        push!(taken_offsets, offset)
                     end
 
-                    update!(main_progress, 
-                            main_progress.counter - prev_p_value + progresses[p].counter)
-                end
 
-                if progresses[p].counter >= lengths[p]
-                    delete!(taken_offsets, progresses[p].offset)
+                    if f == PP_NEXT
+                        if count_overshoot || progresses[p].counter < lengths[p]
+                            next!(progresses[p], args...; kw...)
+                            mainprogress && next!(main_progress)
+                        end
+                    else
+                        prev_p_value = progresses[p].counter
+                        
+                        if f == PP_FINISH
+                            finish!(progresses[p], args...; kw...)
+                        elseif f == PP_CANCEL
+                            finish!(progresses[p])
+                            cancel(progresses[p], args...; kw...)
+                        elseif f == PP_UPDATE
+                            if !isempty(args)
+                                value = args[1]
+                                value == (:) && (value = progresses[p].counter)
+                                !count_overshoot && (value = min(value, lengths[p]))
+                                update!(progresses[p], value, args[2:end]...; kw...)
+                            else
+                                update!(progresses[p]; kw...)
+                            end
+                        end
+
+                        mainprogress && update!(main_progress, 
+                                main_progress.counter - prev_p_value + progresses[p].counter)
+                    end
+
+                    if progresses[p].counter >= lengths[p]
+                        delete!(taken_offsets, progresses[p].offset)
+                    end
                 end
             end
-        end
-        while isready(mp.channel)
-            take!(mp.channel)
+        catch e
+            println("ERROR")
+            println(e)
         end
         mp.channel = FakeChannel()
+        while isready(channel)
+            take!(channel)
+        end
+        close(channel)
         print("\n" ^ max_offsets)
     end
 
--- test/test_parallel.jl
+++ test/test_parallel.jl
@@ -1,16 +1,13 @@
 using Distributed
 using ProgressMeter: FakeChannel
 
-if workers() != [1]
-    rmprocs(workers())
-end
-addprocs(4)
+nworkers() == 1 && addprocs(4)
 @everywhere using ProgressMeter
 
 @testset "ParallelProgress() tests" begin
 
     procs = nworkers()
-    (procs == 1) && @info "incomplete tests: nworkers() == 1"
+    procs == 1 && @info "incomplete tests: nworkers() == 1"
     @test all([@fetchfrom w @isdefined(ProgressMeter) for w in workers()])
 
     println("Testing simultaneous updates")
@@ -22,6 +19,22 @@ addprocs(4)
         end
     end
     sleep(0.1)
+    @test p.channel isa FakeChannel #ParallelProgress finished
+
+    println("Testing update!")
+    prog = Progress(100)
+    p = ParallelProgress(prog)
+    for _ in 1:25
+        sleep(0.05)
+        next!(p)
+    end
+    update!(p, 75)
+    for _ in 76:100
+        sleep(0.05)
+        next!(p)
+    end
+    sleep(0.1)
+    @test p.channel isa FakeChannel #ParallelProgress finished
 
     println("Testing over-shooting")
     p = ParallelProgress(10)
@@ -83,84 +96,71 @@ addprocs(4)
     sleep(0.1)
     @test p.channel isa FakeChannel #ParallelProgress finished
     @test length(unique(ids)) == procs
-end
 
-@testset "MultipleProgress() tests" begin
-
-    procs = nworkers()
-    @test all([@fetchfrom w @isdefined(ProgressMeter) for w in workers()])
-
-    function test_MP_finished(lengths, finish; kwargs...)
-        n = length(lengths)
-        p = MultipleProgress(lengths; kwargs...)
-        for _ in 1:100
-            sleep(0.01)
-            for i in 1:n
-                next!(p[i])
-            end
-        end
-        finish && finish!(p)
-        sleep(0.1)
-        for i in 1:n
-            @test p[i].channel.channel isa FakeChannel 
+    println("Testing changing color with next! and update!")
+    p = ParallelProgress(100)
+    for i in 1:100
+        sleep(0.05)
+        if i == 25
+            next!(p, :red)
+        elseif i == 50
+            update!(p, 51, :blue)
+        else
+            next!(p)
         end
     end
-
-    println("Testing custom titles and color")
-    test_MP_finished([100, 100], false; 
-                     desc="yellow  ", color=:yellow, 
-                     kws=[(desc=" task A ",), (desc=" task B ",)])
-
-    println("Testing over-shooting and under-shooting")
-    test_MP_finished([10, 110], true; dt=0.01)
-
-    println("Testing over-shooting with count_overshoot")
-    test_MP_finished([10, 190], false; count_overshoot=true, dt=0.01)
-
-    println("Testing rapid over-shooting")
-    p = MultipleProgress([100]; dt=0.01, count_overshoot=true)
-    next!(p[1])
     sleep(0.1)
-    for _ in 1:10000
-        next!(p[1])
+    @test p.channel isa FakeChannel #ParallelProgress finished
+
+    println("Testing changing desc with next! and update!")
+    p = ParallelProgress(100)
+    for i in 1:100
+        sleep(0.05)
+        if i == 25
+            next!(p, desc="25% done ")
+        elseif i == 50
+            update!(p, 51, desc="50% done ")
+        else
+            next!(p)
+        end
     end
     sleep(0.1)
-    @test p[1].channel.channel isa FakeChannel #ParallelProgress finished
+    @test p.channel isa FakeChannel # ParallelProgress finished
 
-    println("Testing early cancel")
-    p = MultipleProgress([100, 80])
-    for _ in 1:50
+    println("Testing with showvalues")
+    p = ParallelProgress(100)
+    for i in 1:100
         sleep(0.02)
-        next!(p[1])
-        next!(p[2])
-    end
-    cancel(p[1])
-    finish!(p[2])
-    sleep(0.1)
-    @test p[1].channel.channel isa FakeChannel #ParallelProgress finished
-
-    println("Testing early global cancel")
-    p = MultipleProgress([100, 80])
-    for _ in 1:50
-        sleep(0.02)
-        next!(p[1])
-        next!(p[2])
-    end
-    cancel(p)
-    sleep(0.1)
-    @test p[2].channel.channel isa FakeChannel #ParallelProgress finished
-
-    println("Testing bar remplacement with $procs workers and pmap")
-    lengths = rand(20:40, 2*procs)
-    p = MultipleProgress(lengths; dt=0.01, kws=[(desc=" task $i   ",) for i in 1:2*procs])
-    ids = pmap(1:2*procs) do ip
-        for _ in 1:lengths[ip]
-            sleep(0.05)
-            next!(p[ip])
+        if i < 50
+            next!(p; showvalues=Dict(:i => i, "longstring" => "ABCD"^i))
+        else
+            next!(p; showvalues=() -> [(:i, "$i"), ("halfdone", true)])
         end
-        myid()
     end
     sleep(0.1)
-    @test length(unique(ids)) == procs
-    @test p[1].channel.channel isa FakeChannel # finished
+    @test p.channel isa FakeChannel # ParallelProgress finished
+
+    println("Testing with ProgressUnknown")
+    p = ParallelProgress(ProgressUnknown())
+    for i in 1:100
+        sleep(0.02)
+        next!(p)
+    end
+    sleep(0.5)
+    update!(p, 200)
+    sleep(0.5)
+    @test !(p.channel isa FakeChannel) # ParallelProgress not finished
+    finish!(p)
+    sleep(0.1)
+    @test p.channel isa FakeChannel # ParallelProgress finished
+
+    println("Testing with ProgressThresh")
+    p = ParallelProgress(ProgressThresh(10))
+    for i in 100:-1:0
+        sleep(0.02)
+        update!(p, i)
+    end
+    sleep(0.1)
+    @test p.channel isa FakeChannel # ParallelProgress finished
+
 end
