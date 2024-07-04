e01e6f5b1d1b2eb128f9482d1d5fb299b71bff53
Leandro Martinez <lmartine@unicamp.br>
2024-07-04 16:42:15 -0300

    remove trailing space

--- src/ProgressMeter.jl
+++ src/ProgressMeter.jl
@@ -81,7 +81,7 @@ Base.@kwdef mutable struct ProgressCore
     numprintedvalues::Int       = 0             # num values printed below progress in last iteration
     prev_update_count::Int      = 1             # counter at last update
     printed::Bool               = false         # true if we have issued at least one status update
-    disable_lock::Bool          = false          # set to false for non-threaded tight loops
+    disable_lock::Bool          = false         # set to false for non-threaded tight loops
     tinit::Float64              = time()        # time meter was initialized
     tlast::Float64              = time()        # time of last update
     tsecond::Float64            = time()        # ignore the first loop given usually uncharacteristically slow
